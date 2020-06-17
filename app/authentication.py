96777f1823c128a5a4963de8ebb0d210ddf906bb
ClassicClips <ClassicClipsWagh@gmail.com>
2020-06-17 05:52:28 -0400

    added status code verification and removed evrything hardcoded

--- app/authentication.py
+++ app/authentication.py
@@ -9,17 +9,15 @@ def get_oauth():
 # verify an oauth code
 def verify_oauth(oauth):
     # curl -H "Authorization: OAuth qczgwwopz7ezmbodw42x1uo88pgjv1" https://id.twitch.tv/oauth2/validate
-    url = 'https://id.twitch.tv/oauth2/validate'
-    ## have to change hard coded header
-    headers = {'Authorization':'OAuth qczgwwopz7ezmbodw42x1uo88pgjv1'}
-    r = requests.get(url,headers=headers)
-    r_dict = r.json()
-    ## have to check if return code is not a 200
-    exp_in = r_dict["expires_in"]
-    if exp_in <= 0:
-        return 0
-    else:
-        return 1
+    current_oauth = 'OAuth ' + config.live_oauth_code
+    headers = {'Authorization':current_oauth}
+    r = requests.get(config.validate_url,headers=headers)
+    if r.status_code != 200:
+        return r.status_code
+    else:    
+        r_dict = r.json()
+        exp_in = r_dict["expires_in"]
+        return 0 if exp_in <= 0 else 1
 
 # refresh oauth code
 def refresh_oauth(oauth):
